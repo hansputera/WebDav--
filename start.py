7ac99234d6bdd9a12df955540ed68e6e218f5df8
hansputera <47862061+hansputera@users.noreply.github.com>
2019-09-11 18:53:04 +0700

    Add files via upload

---
+++ start.py
@@ -0,0 +1,38 @@
+import os
+import time
+import sys
+
+if "linux" in sys.platform:
+	print "Can't Reload This Page !"
+	else:
+		
+def menu():
+	os.system("cls")
+	time.sleep(2)
+	print""" 
+	 __      __       .__                                
+/  \    /  \ ____ |  |   ____  ____   _____   ____   
+\   \/\/   // __ \|  | _/ ___\/  _ \ /     \_/ __ \  
+ \  v1.0   /\  ___/|  |_\  \__(  <_> )  Y Y  \  ___/  
+  \__/\  /  \___  >____/\___  >____/|__|_|  /\___  > 
+       \/       \/          \/            \/     \/  
+       
+      >> WINDOWS USER >>
+
+"""
+time.sleep(2)
+os.system("cls")
+print "[!] Starting webDav [!]"
+time.sleep(3)
+os.system("cls")
+print """
+
+Usage : python2 webs.py namewebsite.com scriptDeface.htm
+
+"""
+
+
+menu()
+
+
+
